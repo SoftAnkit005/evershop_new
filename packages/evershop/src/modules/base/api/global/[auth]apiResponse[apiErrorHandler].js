8b3a697b9f365a996b084ffb3b2e0de5e4948958
SoftAnkit005 <ankitdonda.softieons@gmail.com>
2024-07-22 14:53:17 +0530

    message

--- packages/evershop/src/modules/base/api/global/[auth]apiResponse[apiErrorHandler].js
+++ packages/evershop/src/modules/base/api/global/[auth]apiResponse[apiErrorHandler].js
@@ -2,6 +2,52 @@
 /* eslint-disable global-require */
 /* eslint-disable guard-for-in */
 /* eslint-disable import/no-import-module-exports */
+
+// ---------------------
+//     Old Module
+// ---------------------
+// const isErrorHandlerTriggered = require('@evershop/evershop/src/lib/middleware/isErrorHandlerTriggered');
+
+// module.exports = async (request, response, delegate, next) => {
+//   /** Get all promise delegate */
+//   const promises = [];
+//   Object.keys(delegate).forEach((id) => {
+//     // Check if middleware is async
+//     if (delegate[id] instanceof Promise) {
+//       promises.push(delegate[id]);
+//     }
+//   });
+
+//   try {
+//     /** Wait for all async middleware to be completed */
+//     await Promise.all(promises);
+
+//     /** If a rejected middleware called next(error) without throwing an error */
+//     if (isErrorHandlerTriggered(response)) {
+
+//     } else {
+//       response.json(response.$body || {});
+//     }
+//   } catch (error) {
+//     if (!isErrorHandlerTriggered(response)) {
+//       next(error);
+//     } else {
+//       // Do nothing here since the next(error) is already called
+//       // when the error is thrown on each middleware
+//     }
+//   }
+// };
+
+
+
+// -------------------
+//    New Module
+// -------------------
+
+/* eslint-disable no-empty */
+/* eslint-disable global-require */
+/* eslint-disable guard-for-in */
+/* eslint-disable import/no-import-module-exports */
 const isErrorHandlerTriggered = require('@evershop/evershop/src/lib/middleware/isErrorHandlerTriggered');
 
 module.exports = async (request, response, delegate, next) => {
@@ -19,13 +65,11 @@ module.exports = async (request, response, delegate, next) => {
     await Promise.all(promises);
 
     /** If a rejected middleware called next(error) without throwing an error */
-    if (isErrorHandlerTriggered(response)) {
-
-    } else {
+    if (!isErrorHandlerTriggered(response) && !response.headersSent) {
       response.json(response.$body || {});
     }
   } catch (error) {
-    if (!isErrorHandlerTriggered(response)) {
+    if (!isErrorHandlerTriggered(response) && !response.headersSent) {
       next(error);
     } else {
       // Do nothing here since the next(error) is already called
