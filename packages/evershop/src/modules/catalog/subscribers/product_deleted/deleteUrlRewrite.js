fdc956158aca9aa362b61ecf2d2c5e4a40c55c83
nitesh-softions <nitesh.softieons@gmail.com>
2024-08-14 14:00:41 +0530

    urlreWrite query restored

--- packages/evershop/src/modules/catalog/subscribers/product_deleted/deleteUrlRewrite.js
+++ packages/evershop/src/modules/catalog/subscribers/product_deleted/deleteUrlRewrite.js
@@ -5,12 +5,8 @@ module.exports = async function buildUrlReWrite(data) {
   const productUuid = data.uuid;
 
   // Delete the url rewrite for the product
-  // await execute(
-  //   pool,
-  //   `DELETE FROM url_rewrite WHERE entity_uuid = ${productUuid} AND entity_type = 'product'`
-  // );
   await execute(
     pool,
-    `SELECT * FROM url_rewrite`
+    `DELETE FROM url_rewrite WHERE entity_uuid = ${productUuid} AND entity_type = 'product'`
   );
 };
